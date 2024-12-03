156ae961dcf06868de34a535e5aa6a8c4476bae1
Joakim Vinter-Olesen <joavin@norlys.dk>
2024-12-03 23:08:23 +0100

    day 3 part 2

--- 03.py
+++ 03.py
@@ -1,5 +1,18 @@
 import re
 
+def sum_results(S):
+    matches = re.findall(r'mul\((\d+),(\d+)\)', S)
+    return sum(int(x)*int(y) for x,y in matches)
+
 with open('input/03.txt') as f:
-    matches = re.findall(r'mul\((\d+),(\d+)\)', f.read())
-    print('Part 1:', sum(int(x)*int(y) for x,y in matches))
+    rest = f.read()
+
+# print('Part 1:', sum_results(rest))
+n = 0
+try:
+    while True:
+        enabled, rest = rest.split(r"don't()", 1)
+        n += sum_results(enabled)
+        disabled, rest = rest.split(r"do()", 1)
+except ValueError:
+    print('Part 2:', n)
