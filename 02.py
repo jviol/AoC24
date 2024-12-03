14eef7e182eb0f725d2e2b101adf048a954093fe
Joakim Vinter-Olesen <joavin@norlys.dk>
2024-12-03 21:39:29 +0100

    day 2 part 2

--- 02.py
+++ 02.py
@@ -10,5 +10,12 @@ def is_safe(levels):
         and all(1 <= abs(levels[i]-levels[i+1]) <= 3
             for i in range(len(levels)-1)))
 
-print('Part 1:', sum(is_safe(levels) for levels in reports))
+# print('Part 1:', sum(is_safe(levels) for levels in reports))
 
+def is_almost_safe(levels):
+    return is_safe(levels) or any(
+        is_safe(levels[:i] + levels[i+1:])
+        for i in range(len(levels))
+    )
+
+print('Part 2:', sum(is_almost_safe(levels) for levels in reports))
