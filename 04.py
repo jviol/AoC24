94301b73255373cd200811184a84b37ec69b7f5e
Joakim Vinter-Olesen <joavin@norlys.dk>
2024-12-04 19:39:50 +0100

    day 4 part 1

---
+++ 04.py
@@ -0,0 +1,27 @@
+
+def transposed_and_reversed(M):
+    return [''.join(M[j][i] for j in range(len(M))) for i in reversed(range(len(M[0])))]
+
+def count_xmases_in_rows(M):
+    return sum(row[i:i + 4] in ["XMAS", "SAMX"]
+               for row in M
+               for i in range(len(row) - 3))
+
+def count_diagonal_xmases(M):
+    return sum(''.join(M[i+k][j+k] for k in range(4)) in ["XMAS", "SAMX"]
+               for i in range(len(M)-3)
+               for j in range(len(M[i])-3))
+
+grid = []
+with open('input/04.txt') as f:
+    for line in f:
+        grid.append(line.strip())
+
+tr_grid = transposed_and_reversed(grid)
+
+print('Part 1:',
+    count_xmases_in_rows(grid)
+      + count_xmases_in_rows(tr_grid)
+      + count_diagonal_xmases(grid)
+      + count_diagonal_xmases(tr_grid))
+
