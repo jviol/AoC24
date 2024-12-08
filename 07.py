e4440b2172bd609b86f840918b7a6d90b69f54f7
Joakim Vinter-Olesen <joakimviol@hotmail.com>
2024-12-08 08:50:03 +0100

    Day 7 Part 2

--- 07.py
+++ 07.py
@@ -7,23 +7,36 @@ def to_str(op):
 def ops_to_str(ops):
     return ' '.join(map(to_str, ops))
 
-def step(n,remaining, test_value):
+def step1(n, remaining, test_value):
     if len(remaining) == 0:
         if n == test_value:
             return n
         else:
             return 0
-    return (step(n + remaining[0], remaining[1:], test_value)
-            or step(n * remaining[0], remaining[1:], test_value))
+    return (step1(n + remaining[0], remaining[1:], test_value)
+            or step1(n * remaining[0], remaining[1:], test_value))
+
+def step2(n, remaining, test_value):
+    if len(remaining) == 0:
+        if n == test_value:
+            return n
+        else:
+            return 0
+    return (step2(n + remaining[0], remaining[1:], test_value)
+            or step2(n * remaining[0], remaining[1:], test_value)
+            or step2(int(f"{n}{remaining[0]}"), remaining[1:], test_value))
 
 
-total = 0
+total1 = 0
+total2 = 0
 with open('input/07.txt') as f:
     for line in f:
         test_value, rest = line.strip().split(': ')
         test_value = int(test_value)
         numbers = list(map(int, rest.split(" ")))
-        total += step(numbers[0], numbers[1:], test_value)
+        total1 += step1(numbers[0], numbers[1:], test_value)
+        total2 += step2(numbers[0], numbers[1:], test_value)
 
-print("Part 1:", total)
+print("Part 1:", total1)
+print("Part 2:", total2)
 
