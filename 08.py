dd4f120a938bbb87dd3cbe5728425f109b0fdeae
Joakim Vinter-Olesen <joakimviol@hotmail.com>
2024-12-08 14:37:04 +0100

    renamed p3 -> p in Part 1

--- 08.py
+++ 08.py
@@ -1,4 +1,6 @@
+import itertools
 from collections import defaultdict
+from itertools import count, takewhile
 
 freq_map = defaultdict(set)
 with open('input/08.txt') as f:
@@ -14,9 +16,9 @@ for positions in freq_map.values():
     for p1 in positions:
         for p2 in positions:
             if p1 != p2:
-                p3 = (2*p1[0]-p2[0], 2*p1[1]-p2[1])
-                if 0 <= p3[0] <= max_y and 0 <= p3[1] <= max_x:
-                    antinodes.add(p3)
+                p = (2 * p1[0] - p2[0], 2 * p1[1] - p2[1])
+                if 0 <= p[0] <= max_y and 0 <= p[1] <= max_x:
+                    antinodes.add(p)
 
 print("Part 1:", len(antinodes))
 
