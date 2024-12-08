e9f6b0be253c55321137ca343f204616912b1de3
Joakim Vinter-Olesen <joakimviol@hotmail.com>
2024-12-08 18:18:29 +0100

    Day 6 Part 2.

--- 06.py
+++ 06.py
@@ -1,13 +1,12 @@
-
 obstructions = set()
 with open('input/06.txt') as f:
     for y, line in enumerate(f):
         for x, c in enumerate(line.strip()):
             if c == '#':
-                obstructions.add((y,x))
+                obstructions.add((y, x))
             elif c == '^':
-                guard_pos = (y,x)
-                guard_dir = (-1, 0) # up
+                starting_guard_pos = (y, x)
+                starting_guard_dir = (-1, 0)  # up
         max_x = x
     max_y = y
 
@@ -16,18 +15,50 @@ def within_bounds(p):
     return 0 <= p[0] <= max_y and 0 <= p[1] <= max_x
 
 
-positions_visited = set()
+def part1():
+    guard_pos = starting_guard_pos
+    guard_dir = starting_guard_dir
+    positions_visited = set()
+    while within_bounds(guard_pos):
+        positions_visited.add(guard_pos)
+        dy, dx = guard_dir
+        y, x = guard_pos
+        new_pos = (y + dy, x + dx)
+        if new_pos in obstructions:
+            guard_dir = (dx, -dy)
+        else:
+            guard_pos = new_pos
+    return positions_visited
 
-while within_bounds(guard_pos):
-    positions_visited.add(guard_pos)
-    dy, dx = guard_dir
-    y, x = guard_pos
-    new_pos = (y + dy, x + dx)
-    if new_pos in obstructions:
-        guard_dir = (dx, -dy)
-    else:
-        guard_pos = new_pos
 
+positions_visited = part1()
 print("Part 1:", len(positions_visited))
 
 
+iterations = 0
+positions_to_try = [p for p in positions_visited if p != starting_guard_pos]
+
+
+def is_loop(obstruction):
+    global iterations, positions_to_try
+    iterations += 1
+    print('\r', iterations, "/", len(positions_to_try), end='', flush=True)
+    obsts = obstructions | {obstruction}
+    guard_pos = starting_guard_pos
+    guard_dir = starting_guard_dir
+    pos_dirs_visited = set()
+    while within_bounds(guard_pos):
+        if (guard_pos, guard_dir) in pos_dirs_visited:
+            return True
+        pos_dirs_visited.add((guard_pos, guard_dir))
+        dy, dx = guard_dir
+        y, x = guard_pos
+        new_pos = (y + dy, x + dx)
+        if new_pos in obsts:
+            guard_dir = (dx, -dy)
+        else:
+            guard_pos = new_pos
+    return False
+
+
+print("\nPart 2:", sum(is_loop(p) for p in positions_to_try))
