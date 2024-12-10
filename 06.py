c5040e5aafa2b119bc85c332157508203e4a7998
Joakim Vinter-Olesen <joakimviol@hotmail.com>
2024-12-10 18:01:21 +0100

    trying to optimize day 6 with a bitarray

--- 06.py
+++ 06.py
@@ -1,21 +1,22 @@
 import time
+from bitarray import bitarray
 from multiprocessing import Pool
 
 start_time = time.time()
-obstructions = set()
+obstructions = bitarray()
 with open('input/06.txt') as f:
     for y, line in enumerate(f):
         for x, c in enumerate(line.strip()):
-            if c == '#':
-                obstructions.add((y, x))
-            elif c == '^':
+            obstructions.append(c == '#')
+            if c == '^':
                 starting_guard_pos = (y, x)
-                starting_guard_dir = (-1, 0)  # up
-        max_x = x
-    max_y = y
+        dim_x = x+1
+    dim_y = y + 1
+    starting_guard_dir = (-1, 0)  # up
+
 
 def within_bounds(p):
-    return 0 <= p[0] <= max_y and 0 <= p[1] <= max_x
+    return 0 <= p[0] < dim_y and 0 <= p[1] <= dim_x
 
 
 def part1(guard_pos, guard_dir):
@@ -25,37 +26,64 @@ def part1(guard_pos, guard_dir):
         dy, dx = guard_dir
         y, x = guard_pos
         new_pos = (y + dy, x + dx)
-        if new_pos in obstructions:
-            guard_dir = (dx, -dy)
-        else:
-            guard_pos = new_pos
+        try:
+            if obstructions[flatten_pos(*new_pos)]:
+                guard_dir = (dx, -dy)
+            else:
+                guard_pos = new_pos
+        except IndexError:
+            return positions_visited
+
     return positions_visited
 
+def flatten_pos(y,x):
+    return y*dim_x+x
+
+def new_empty_bitarray():
+    b = bitarray(dim_y*dim_x)
+    b.setall(0)
+    return b
 
 def is_loop(obstruction):
-    obsts = obstructions | {obstruction}
+    obstructions[obstruction[0]*dim_x+obstruction[1]] = True
+    # obsts = obstructions[:]
+    # obsts[obstruction[0]*dim_x+obstruction[1]] = True
     guard_pos = starting_guard_pos
     guard_dir = starting_guard_dir
-    pos_dirs_visited = set()
-    while within_bounds(guard_pos):
-        if (guard_pos, guard_dir) in pos_dirs_visited:
-            return True
-        pos_dirs_visited.add((guard_pos, guard_dir))
-        dy, dx = guard_dir
-        y, x = guard_pos
-        new_pos = (y + dy, x + dx)
-        if new_pos in obsts:
-            guard_dir = (dx, -dy)
-        else:
-            guard_pos = new_pos
-    return False
+    pos_dirs_visited = {
+        (-1,0):new_empty_bitarray(),
+        (1,0):new_empty_bitarray(),
+        (0,-1):new_empty_bitarray(),
+        (0,1):new_empty_bitarray(),
+    }
+    try:
+        while within_bounds(guard_pos):
+            pos = flatten_pos(*guard_pos)
+            if pos_dirs_visited[guard_dir][pos]:
+                return True
+            y, x = guard_pos
+            pos_dirs_visited[guard_dir][pos] = True
+            dy, dx = guard_dir
+            new_pos = y_new, x_new = (y + dy, x + dx)
+
+            if obstructions[y_new*dim_x+x_new]:
+                guard_dir = (dx, -dy)
+            else:
+                guard_pos = new_pos
+        return False
+    except IndexError:
+        return False
+    finally:
+        obstructions[obstruction[0] * dim_x + obstruction[1]] = False
+
+
 
 if __name__ == '__main__':
     positions_visited = part1(starting_guard_pos, starting_guard_dir)
     print("Part 1:", len(positions_visited))
 
     positions_to_try = [p for p in positions_visited if p != starting_guard_pos]
-    with Pool() as pool:
-        print("\nPart 2:", sum(pool.map(is_loop, positions_to_try)), "Time:", time.time() - start_time, "sec.")
-
+    # with Pool() as pool:
+    print("\nPart 2:", sum(map(is_loop, positions_to_try)))
+    print(f"Time: {time.time() - start_time:.2f} sec.")
 
