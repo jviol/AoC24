bebb8f3962e12d6678a13139a1a48a44c2599744
Joakim Vinter-Olesen <joakim.vinter.olesen@gmail.com>
2024-12-17 21:34:21 +0100

    Day 14 Part 2 wip

--- 14_2.py
+++ 14_2.py
@@ -1,10 +1,9 @@
+import math
 import re
 import matplotlib.pyplot as plt
 import numpy as np
+from bitarray import bitarray, frozenbitarray
 
-
-
-q = [0, 0, 0, 0]
 w = 101
 h = 103
 ps = []
@@ -39,27 +38,70 @@ def step(n):
 
 
 def print_picture():
-    picture = [[' ']*w for _ in range(h)]
-    for (x,y) in ps:
-        picture[y][x] = '#'
-    print('\n'.join(''.join(line) for line in picture))
+    # Clear any existing plots
+    plt.clf()
 
+    # Create canvas and plot points
+    canvas = np.ones((h, w))
+    for (x, y) in ps:
+        canvas[y, x] = 0
+
+    # Display
+    plt.imshow(canvas, cmap='binary')
+    plt.axis('off')
+    plt.draw()
+    plt.pause(0.1)  # Give time to render
 
 def is_symmetrical():
     in_left_side = sum(x < w // 2 for x,y in ps)
     in_right_side = sum(x > w // 2 for x,y in ps)
     return in_left_side == in_right_side
 
-i = 0
-while True:
-    while not is_symmetrical():
+def ps_as_bitarray():
+    a = bitarray(w*h)
+    for x,y in ps:
+        a[y*w+x] = True
+    return frozenbitarray(a)
+
+
+def detect_cycle():
+    i = 0
+    patterns = set()
+    a = ps_as_bitarray()
+    while not a in patterns:
+        patterns.add(a)
         i += 1
         step(1)
+        a = ps_as_bitarray()
     print("Iteration", i)
     print_picture()
     input("continue..?")
+
+def safety_factor():
+    qs = [0, 0, 0, 0]
+    for x,y in ps:
+        quadrant = get_quadrant(x, y)
+        if quadrant:
+            qs[quadrant]+=1
+    return math.prod(qs)
+
+i = 0
+step(1)
+min_sf = safety_factor()
+while True:
+    print(min_sf)
     i += 1
     step(1)
+    sf = safety_factor()
+    if sf < min_sf:
+        print("Iteration", i)
+        print_picture()
+        input("continue..?")
+        min_sf = sf
+
+
+
+
 
 
 
